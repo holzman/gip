19086cb1b94aa52bd41fef94b6b19a69c7543add
Brian Bockelman <bbockelm@cse.unl.edu>
2008-02-20 01:53:53 +0000

    Added generated HTML to ignore.

--- test/generate_docs.py
+++ test/generate_docs.py
@@ -3,10 +3,12 @@
 import sys
 import os
 
+
 sys.argv = ['epydoc', 'gip_common', 'condor_common', 'dCacheAdmin', 
             'gip_storage', 'gip_testing', 'ldap', 'pbs_common', 'user_input']
 
 sys.path.append(os.path.expandvars("$GIP_LOCATION/lib/python"))
+
 from epydoc.cli import cli
 cli()
 

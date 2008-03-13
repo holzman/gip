fbc7887d283793cc2bab1b00fdfed7e2f18fec3a
Anthony Tiradani <tiradani@fnal.gov>
2008-03-13 18:55:58 +0000

    Modified Main to handle custom streams for logging the test results

--- test/pbs_test.py
+++ test/pbs_test.py
@@ -8,26 +8,27 @@ from sets import Set
 sys.path.append(os.path.expandvars("$GIP_LOCATION/lib/python"))
 from gip_common import config
 from pbs_common import getVoQueues
+from gip_testing import runTest, streamHandler
 
-example_queues = [('osg', 'default'), ('osgedu', 'default'), 
-    ('mis', 'default'),      ('fmri', 'default'),    ('grase', 'default'), 
-    ('gridex', 'default'),   ('ligo', 'default'),    ('ivdgl', 'default'), 
-    ('gadu', 'default'),     ('glow', 'default'),    ('cdf', 'default'), 
-    ('nanohub', 'default'),  ('sdss', 'default'),    ('gpn', 'default'), 
-    ('engage', 'default'),   ('dteam', 'default'),   ('ops', 'default'), 
-    ('cms', 'lcgadmin'),     ('osg', 'lcgadmin'),    ('osgedu', 'lcgadmin'), 
-    ('mis', 'lcgadmin'),     ('fmri', 'lcgadmin'),   ('grase', 'lcgadmin'), 
-    ('gridex', 'lcgadmin'),  ('ligo', 'lcgadmin'),   ('ivdgl', 'lcgadmin'), 
-    ('gadu', 'lcgadmin'),    ('glow', 'lcgadmin'),   ('cdf', 'lcgadmin'), 
-    ('nanohub', 'lcgadmin'), ('dzero', 'lcgadmin'),  ('sdss', 'lcgadmin'), 
-    ('gpn', 'lcgadmin'),     ('engage', 'lcgadmin'), ('atlas', 'lcgadmin'), 
-    ('dteam', 'lcgadmin'),   ('ops', 'lcgadmin'),    ('cms', 'cmsprod'), 
-    ('atlas', 'atlas'),      ('osg', 'workq'),       ('osgedu', 'workq'), 
-    ('mis', 'workq'),        ('fmri', 'workq'),      ('grase', 'workq'), 
-    ('gridex', 'workq'),     ('ligo', 'workq'),      ('ivdgl', 'workq'), 
-    ('gadu', 'workq'),       ('glow', 'workq'),      ('cdf', 'workq'), 
-    ('nanohub', 'workq'),    ('sdss', 'workq'),      ('gpn', 'workq'), 
-    ('engage', 'workq'),     ('dteam', 'workq'),     ('ops', 'workq'), 
+example_queues = [('osg', 'default'), ('osgedu', 'default'),
+    ('mis', 'default'),      ('fmri', 'default'),    ('grase', 'default'),
+    ('gridex', 'default'),   ('ligo', 'default'),    ('ivdgl', 'default'),
+    ('gadu', 'default'),     ('glow', 'default'),    ('cdf', 'default'),
+    ('nanohub', 'default'),  ('sdss', 'default'),    ('gpn', 'default'),
+    ('engage', 'default'),   ('dteam', 'default'),   ('ops', 'default'),
+    ('cms', 'lcgadmin'),     ('osg', 'lcgadmin'),    ('osgedu', 'lcgadmin'),
+    ('mis', 'lcgadmin'),     ('fmri', 'lcgadmin'),   ('grase', 'lcgadmin'),
+    ('gridex', 'lcgadmin'),  ('ligo', 'lcgadmin'),   ('ivdgl', 'lcgadmin'),
+    ('gadu', 'lcgadmin'),    ('glow', 'lcgadmin'),   ('cdf', 'lcgadmin'),
+    ('nanohub', 'lcgadmin'), ('dzero', 'lcgadmin'),  ('sdss', 'lcgadmin'),
+    ('gpn', 'lcgadmin'),     ('engage', 'lcgadmin'), ('atlas', 'lcgadmin'),
+    ('dteam', 'lcgadmin'),   ('ops', 'lcgadmin'),    ('cms', 'cmsprod'),
+    ('atlas', 'atlas'),      ('osg', 'workq'),       ('osgedu', 'workq'),
+    ('mis', 'workq'),        ('fmri', 'workq'),      ('grase', 'workq'),
+    ('gridex', 'workq'),     ('ligo', 'workq'),      ('ivdgl', 'workq'),
+    ('gadu', 'workq'),       ('glow', 'workq'),      ('cdf', 'workq'),
+    ('nanohub', 'workq'),    ('sdss', 'workq'),      ('gpn', 'workq'),
+    ('engage', 'workq'),     ('dteam', 'workq'),     ('ops', 'workq'),
     ('cms', 'cms'),          ('dzero', 'dzero')]
 example_queues = Set(example_queues)
 
@@ -53,7 +54,14 @@ class TestPbsDynamic(unittest.TestCase):
         diff = vo_queues.symmetric_difference(example_queues)
         self.assertEquals(len(diff), 0, msg="The following VO-queues are " \
             "different between the expected and actual: %s" % str(diff))
-        
+
+def main():
+    """
+    The main entry point for when ce_print is run in standalone mode.
+    """
+    cp = config()
+    stream = streamHandler(cp)
+    runTest(cp, TestPbsDynamic, stream)
 
 if __name__ == '__main__':
-    unittest.main()
+    main()

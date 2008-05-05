9099615a2a50f8b83309f7974af0ba8380885f3f
Brian Bockelman <bbockelm@cse.unl.edu>
2008-05-05 18:28:09 +0000

    Merged latest trunk patched into the 1.0.0 branch.

--- gip/lib/python/gip_testing.py
+++ gip/lib/python/gip_testing.py
@@ -9,11 +9,13 @@ outputs from the test/command_output directory.
 import os
 import re
 import sys
+import types
 import unittest
 import datetime
+import urlparse
 
 from gip_common import cp_get, pathFormatter, parseOpts
-from gip_ldap import getSiteList
+from gip_ldap import getSiteList, prettyDN
 
 replace_command = False
 
@@ -131,6 +133,19 @@ class GipValidate(unittest.TestCase):
     def run(self):
         self.test_existence_all()
         self.test_egee_site_unique_id()
+        self.test_ce()
+        self.test_site()
+        self.test_missing_newlines()
+
+    def test_missing_newlines(self):
+        r = re.compile("Glue\w*:\s")
+        for entry in self.entries:
+            for key, val in entry.glue.items():
+                if not isinstance(val, types.StringType):
+                    continue
+                m = r.search(val)
+                self.assertEquals(m, None, msg="Entry %s, key %s is missing" \
+                    " the newline character." % (prettyDN(entry.dn), key))
 
     def test_existence_all(self):
         self.test_existence("GlueCEUniqueID")
@@ -146,23 +161,66 @@ class GipValidate(unittest.TestCase):
         self.test_existence("GlueSEControlProtocolLocalID")
         self.test_existence("GlueSALocalID")
 
-    def test_existence(self, name):
+    def test_ce(self):
+        self.test_value_not_equal("GlueCE", "CEInfoDefaultSE", "UNAVAILABLE")
+        self.test_value_not_equal("GlueCE", "CEPolicyMaxCPUTime", "0")
+        self.test_value_not_equal("GlueCE", "CEInfoTotalCPUs", "0")
+        self.test_value_not_equal("GlueCE", "CEStateEstimatedResponseTime", "0")
+        self.test_value_not_equal("GlueCE", "CEStateWorstResponseTime", "0")
+
+    def test_site(self):
+        self.test_url()
+        self.test_sponsors()
+
+    def test_url(self):
         for entry in self.entries:
-            if entry.dn[0].startswith(name):
+            if 'GlueSite' not in entry.objectClass:
+                continue
+            parts= urlparse.urlparse(entry.glue['SiteWeb'])
+            self.assertNotEquals(parts, '', msg="Invalid website: %s" % \
+                entry.glue['SiteWeb'])
+           
+    def test_sponsors(self):
+        r = re.compile("(\w+):([0-9]+)")
+        for entry in self.entries:
+            if 'GlueSite' not in entry.objectClass:
+                continue
+            m = r.findall(entry.glue['SiteSponsor'])
+            self.assertNotEquals(m, None, msg="Invalid site sponsor: %s" % \
+                entry.glue['SiteSponsor'])
+            tot = 0
+            for e in m:
+                tot += int(e[1])
+            self.assertEquals(tot, 100, msg="Site sponsorship does not add up "\
+                " to 100: %s" % entry.glue['SiteSponsor'])
+
+    def test_existence(self, name, full=False):
+        for entry in self.entries:
+            if full and entry.dn[0] == name:
+                return
+            if (not full) and entry.dn[0].startswith(name):
                 return
         self.assertEquals(0, 1, msg="GLUE Entity %s does not exist." % name)
 
     def test_chunk_keys(self):
-        for entry in entries:
+        for entry in self.entries:
             if 'ChunkKey' not in entry.glue:
                 continue
-            self.test_existence(entry.glue['ChunkKey'])
+            self.test_existence(entry.glue['ChunkKey'], full=True)
         
     def test_egee_site_unique_id(self):
-        for entry in entries:
+        for entry in self.entries:
             if 'GlueSite' not in entry.objectClass:
                 continue
             self.assertEquals(entry.glue['SiteName'], \
-                entry.glue['SiteUniqueID'], msg="For EGEE compat., must have" \
+                entry.glue['SiteUniqueID'], msg="For EGEE compat., must have " \
                 "GlueSiteName == GlueSiteUniqueID")
+
+    def test_value_not_equal(self, objClass, attribute, value):
+        for entry in self.entries:
+            if objClass not in entry.objectClass:
+                continue
+            self.assertNotEquals(entry.glue[attribute], value, msg="GLUE " \
+                "attribute %s for entity %s is equal to %s" % (attribute, \
+                objClass, value))
 

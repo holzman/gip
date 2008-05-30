671442664ba45c68e8c3e2024a2a049e82163004
Brian Bockelman <bbockelm@cse.unl.edu>
2008-05-30 02:46:01 +0000

    Addition of some unit tests for the new info-wrapper.

--- test/osg_info_wrapper_test.py
+++ test/osg_info_wrapper_test.py
@@ -1,7 +1,16 @@
 
+import os
+import sys
 import unittest
 
+#Standard testing imports:
+sys.path.append(os.path.expandvars("$GIP_LOCATION/lib/python"))
 from gip_common import config
+from gip_testing import runTest, streamHandler
+
+#Add the path with the osg_info_wrapper script:
+sys.path.append(os.path.expandvars("$GIP_LOCATION/libexec"))
+import osg_info_wrapper
 
 class TestOsgInfoWrapper(unittest.TestCase):
 
@@ -10,7 +19,21 @@ class TestOsgInfoWrapper(unittest.TestCase):
         Simple test of the OSG Info Wrapper.  Make sure that both the provider
         and plugin functionality works.
         """
-        raise NotImplementedError()
+        cp = config("test_modules/simple/config")
+        entries = osg_info_wrapper.main(cp, return_entries=True)
+        has_timestamp = False
+        has_ce = False
+        for entry in entries:
+            print entry
+            if entry.glue['LocationName'][0] == 'TIMESTAMP':
+                has_timestamp = True
+            if entry.dn[0] == 'GlueCEUniqueID=red.unl.edu:2119/jobmanager' \
+                    '-pbs-workq':
+                has_ce = True
+                self.assertEquals(entry.glue['CEPolicyAssignedJobSlots'][0], \
+                    '1234', msg="Plugin did not get applied properly")
+        self.assertEquals(has_timestamp, True, msg="Provider did not run.")
+        self.assertEquals(has_ce, True, msg="Static info was not included.")
 
     def test_timeout(self):
         """
@@ -52,14 +75,27 @@ class TestOsgInfoWrapper(unittest.TestCase):
         """
         Make sure that osg-info-wrapper flushes the cache properly.
         """
-        cp = config("test_modules/simple/config")
+        cp = config("test_modules/cache_flush/config")
         cp.set("gip", "flush_cache", "False")
-        entries = osg_info_wrapper.main(cp, return_entries)
+        entries = osg_info_wrapper.main(cp, return_entries=True)
         timestamp_entry = entries[0]
-        t1 = float(timestamp_entry.glue['LocationVersion'])
+        t1 = float(timestamp_entry.glue['LocationVersion'][0])
         cp.set("gip", "flush_cache", "True")
-        entries = osg_info_wrapper.main(cp, return_entries)
+        entries = osg_info_wrapper.main(cp, return_entries=True)
         timestamp_entry = entries[0]
-        t2 = float(timestamp_entry.glue['LocationVersion'])
+        t2 = float(timestamp_entry.glue['LocationVersion'][0])
+        print t1, t2
         self.assertTrue(t1 < t2)
+
+def main():
+    """
+    The main entry point for testing the osg-info-wrapper implementation.
+    """
+    cp = config()
+    stream = streamHandler(cp)
+    runTest(cp, TestOsgInfoWrapper, stream, per_site=False)
+
+if __name__ == '__main__':
+    main()
+
 

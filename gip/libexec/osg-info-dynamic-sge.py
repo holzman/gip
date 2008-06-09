25c55a6cb9217d77286c3b6b3532ce95a9351397
Anthony Tiradani <tiradani@fnal.gov>
2008-06-09 19:10:46 +0000

    Checking in code so changes are not lost.  This code has not been tested yet.

--- gip/libexec/osg-info-dynamic-sge.py
+++ gip/libexec/osg-info-dynamic-sge.py
@@ -21,66 +21,53 @@ def now():
 def print_CE(cp):
     sgeVersion = getLrmsInfo(cp)
     queueInfo = getQueueInfo(cp)
-    totalCpu, freeCpu, queueCpus = parseNodes(cp, sgeVersion)
     ce_name = cp.get("ce", "name")
     CE_plugin = getTemplate("GlueCEPlugin", "GlueCEUniqueID")
     try:
-        excludeQueues = [i.strip() for i in cp.get("sqe", "queue_exclude").split(',')]
+        excludeQueues = [i.strip() for i in cp.get("sge", "queue_exclude").split(',')]
     except:
         excludeQueues = []
+
     for queue, info in queueInfo.items():
         if queue in excludeQueues:
             continue
         info["version"] = sgeVersion
         info["job_manager"] = "sge"
-        if info["wait"] > 0:
-            info["free_slots"] = 0
-        else:
-            if queue in queueCpus:
-                info["free_slots"] = queueCpus[queue]
-            else:
-                info["free_slots"] = freeCpu
-        info["queue"] = queue
         info["ce_name"] = ce_name
-        if "job_slots" not in info:
-            info["job_slots"] = totalCpu
-        if "priority" not in info:
-            info["priority"] = 0
-        if "max_running" not in info:
-            info["max_running"] = info["job_slots"]
-        if "max_wall" not in info:
-            info["max_wall"] = 0
-        info["job_slots"] = min(totalCpu, info["job_slots"])
+
         print CE_plugin % info
-    return queueInfo, totalCpu, freeCpu, queueCpus
+    return queueInfo
 
 def print_VOViewLocal(queue_info, cp):
     ce_name = cp.get("ce", "name")
-    vo_map = VoMapper(cp)
-    queue_jobs = getJobsInfo(vo_map, cp)
     VOView_plugin = getTemplate("GlueCEPlugin", "GlueVOViewLocalID")
     try:
         queue_exclude = [i.strip() for i in cp.get("sge", "queue_exclude").split(',')]
     except:
         queue_exclude = []
-    for queue, vo_info in queue_jobs.items():
-        if queue in queue_exclude:
+
+    for queue, info in queue_info.items():
+        queue_jobs = info["job_data"]
+
+        if jobs_info["queue"] in queue_exclude:
             continue
+
         try:
             whitelist = [i.strip() for i in cp.get("sge", "%s_whitelist" % queue).split(',')]
         except:
             whitelist = []
+
         try:
             blacklist = [i.strip() for i in cp.get("sge", "%s_blacklist" % queue).split(',')]
         except:
             blacklist = []
-        for vo, info in vo_info.items():
+
+        for vo, info in queue_jobs.items():
             if (vo in blacklist or "*" in blacklist) and vo not in whitelist:
                 continue
             info["job_slots"] = queue_info.get(queue, {}).get("job_slots", 0)
             info["free_slots"] = queue_info.get(queue, {}).get("free_slots", 0)
             info["ce_name"] = ce_name
-            info["queue"] = queue
             info["vo"] = vo
             info["job_manager"] = "sge"
             print VOView_plugin % info
@@ -89,7 +76,7 @@ def main():
     try:
         cp = config()
         addToPath(cp.get("sge", "sge_path"))
-        queueInfo, totalCpu, freeCpu, queueCpus = print_CE(cp)
+        queueInfo = print_CE(cp)
         print_VOViewLocal(queueInfo, cp)
     except Exception, e:
         log.error(e)

ba2d9f3c38823e17d4bced937345a91195019e0c
Anthony Tiradani <tiradani@fnal.gov>
2008-06-17 14:47:33 +0000

    Added parseXmlSax for xml parsing using the Sax parser

--- gip/lib/python/xml_common.py
+++ gip/lib/python/xml_common.py
@@ -1,5 +1,7 @@
 import xml.dom.minidom
 from gip_common import fileRead
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler, feature_external_ges
 
 def getText(nodelist):
     rc = ""
@@ -16,3 +18,22 @@ def getDom(source, sourcetype="string"):
         dom = xml.dom.minidom.parseString(fileRead(source))
 
     return dom
+
+def parseXmlSax(fp, handler):
+    """
+    Parse XML using the SAX parser.
+
+    Create a SAX parser with the content handler B{handler}, then parse the
+    contents of B{fp} with it.
+
+    @param fp: A file-like object of the Condor XML data
+    @param handler: An object which will be our content handler.
+    @type handler: xml.sax.handler.ContentHandler
+    @returns: None
+    """
+    parser = make_parser()
+    parser.setContentHandler(handler)
+    parser.setFeature(feature_external_ges, False)
+    parser.parse(fp)
+
+

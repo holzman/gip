b55738f675dfa4b2d390fb3a9242ef6714fe9659
Anthony Tiradani <tiradani@fnal.gov>
2008-06-17 14:49:19 +0000

    Sax parsing classes for the SGE xml output

---
+++ gip/lib/python/sge_sax_handler.py
@@ -0,0 +1,90 @@
+from xml.sax.handler import ContentHandler
+
+class QueueInfoParser(ContentHandler):
+    def __init__(self):
+        self.currentQueueInfoElmList = List(['name', 'qtype', 'slots_used', 'slots_total', 'arch'])
+        self.currentJobInfoElmList = List(['JB_job_number', 'JB_job_number', 'JAT_prio', 'JB_name', \
+            'JB_owner', 'state', 'JAT_start_time', 'JB_submission_time', 'slots'])
+
+    def startDocument(self):
+        self.elmContents = ''
+        self.QueueList = {}
+
+    def startElement(self, name, attrs):
+        if name == 'Queue-List':
+            self.currentQueueInfo = {}
+            self.currentJobList = {}
+            self.currentJobInfo = {}
+
+        elif name == 'job_info':
+            self.QueueList["waiting"] = {}
+            self.currentJobList = {}
+            self.inJobInfo = True
+
+        elif name == 'queue_info':
+            self.inJobInfo = False
+
+        elif name == 'job_list':
+            self.currentJobInfo = {}
+
+        else:
+            pass
+
+    def endElement(self, name):
+        import copy
+        if name == 'Queue-List':
+            self.currentQueueInfo['jobs'] = copy.deepcopy(self.currentJobList)
+            self.QueueList[self.currentQueueInfo['name']] = copy.deepcopy(self.currentQueueInfo)
+
+        elif name == 'job_list':
+            self.currentJobList[self.currentJobInfo['JB_job_number']] = copy.deepcopy(self.currentJobInfo)
+
+        elif name == 'job_info':
+            if self.inJobInfo:
+                self.QueueList['waiting'] = copy.deepcopy(self.currentJobList)
+
+        elif name in self.currentQueueInfoElmList:
+            self.currentQueueInfo[name] = str(self.elmContents)
+
+        elif name in self.currentJobInfoElmList:
+            self.currentJobInfo[name] = str(self.elmContents)
+
+        else:
+            pass
+
+    def characters(self, ch):
+        self.elmContents += str(ch)
+
+    def getQueueInfo(self):
+        return self.QueueList
+
+
+class JobInfoParser(ContentHandler):
+    def __init__(self):
+        self.currentJobInfoElmList = List(['JB_job_number', 'JAT_prio', 'JB_name', \
+            'JB_owner', 'state', 'JAT_start_time', 'JB_submission_time', 'slots'])
+
+    def startDocument(self):
+        self.elmContents = ''
+        self.JobList = []
+
+    def startElement(self, name, attrs):
+        if name == 'job_list':
+            self.currentJobInfo = {}
+        else:
+            pass
+
+    def endElement(self, name):
+        import copy
+        if name == 'job_list':
+            self.JobList.append(copy.deepcopy(self.currentJobInfo))
+        elif name in self.currentJobInfoElmList:
+            self.currentJobInfo[name] = str(self.elmContents)
+        else:
+            pass
+
+    def characters(self, ch):
+        self.elmContents += str(ch)
+
+    def getQueueInfo(self):
+        return self.JobList

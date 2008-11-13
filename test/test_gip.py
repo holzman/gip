6dffa9ae9537a2f6c7d07ea1f5db9aebcb54f27a
Brian Bockelman <bbockelm@cse.unl.edu>
2008-11-13 18:08:22 +0000

    Unit test for subcluster generation using the OSG 1.0 config format.

--- test/test_gip.py
+++ test/test_gip.py
@@ -6,6 +6,8 @@ from test_gip_common import main as gip_main
 from test_topology import main as topology_main
 from osg_info_wrapper_test import main as wrapper_main
 from test_classicSE import main as classicSE_main
+from test_cesebind import main as cese_main
+from test_subclusters import main as subclusters_main
 
 def test_run(fcn):
     try:
@@ -21,6 +23,8 @@ def main():
     test_run(topology_main)
     test_run(classicSE_main)
     test_run(wrapper_main)
+    test_run(cese_main)
+    test_run(subclusters_main)
 
 if __name__ == '__main__':
     main()

904cf2be5799e1037ec4946043a6c37eb0d1d654
Burt Holzman <burt@fnal.gov>
2009-01-22 20:18:03 +0000

    Fix missing import and remove datetime import (python 2.2)

--- gip/libexec/token_info_provider.py
+++ gip/libexec/token_info_provider.py
@@ -4,11 +4,11 @@ import time
 import re
 import os
 import sys
-import datetime
+#import datetime
 import ConfigParser
 
 sys.path.append(os.path.expandvars("$GIP_LOCATION/lib/python"))
-from gip_common import config, getTemplate, getLogger, cp_get
+from gip_common import config, getTemplate, getLogger, cp_get, cp_getBoolean
 from gip_storage import execute, connect, connect_admin, voListStorage, \
     getPath, getSESpace, getSETape
 
@@ -164,7 +164,8 @@ def print_SA(p, cp):
     database.
     """
 
-    command = SA_command % (cp.getint('dcache_config', 'min_lifetime')*1000)
+    min_lifetime = int(cp_get(cp, 'dcache_config', 'min_lifetime', 60))
+    command = SA_command % (min_lifetime*1000)
     rows=execute(p, command)
     saTemplate = getTemplate("GlueSE", "GlueSALocalID")
 

415e6cf7ddfc06e9c39686fa621a3c7a2aa13241
Brian Bockelman <bbockelm@cse.unl.edu>
2009-05-08 20:47:10 +0000

    Fix for ticket #41 where there are multiple non-unique CESEBind groups.

--- gip/lib/python/gip/providers/cese_bind.py
+++ gip/lib/python/gip/providers/cese_bind.py
@@ -7,15 +7,23 @@ import os
 sys.path.append(os.path.expandvars("$GIP_LOCATION/lib/python"))
 from gip_common import config, getLogger, getTemplate, printTemplate
 from gip_cese_bind import getCESEBindInfo
+import gip_sets as sets
 
 log = getLogger("GIP.CESEBind")
 
 def print_CESEBind(cp):
     group_template = getTemplate("GlueCESEBind", "GlueCESEBindGroupCEUniqueID")
     se_template = getTemplate("GlueCESEBind", "GlueCESEBindSEUniqueID")
-    for info in getCESEBindInfo(cp):
-        printTemplate(group_template, info)
+    bind_info = getCESEBindInfo(cp)
+    cegroups = {}
+    for info in bind_info:
         printTemplate(se_template, info)
+        ses = cegroups.setdefault(info['ceUniqueID'], sets.Set())
+        ses.add(info['seUniqueID'])
+    for ce, ses in cegroups.items():
+        ses = '\n'.join(['GlueCESEBindGroupSEUniqueID: %s' % i for i in ses])
+        info = {'ceUniqueID': ce, 'se_groups': ses}
+        printTemplate(group_template, info)
 
 def main():
     try:

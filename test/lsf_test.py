55a081438196fcf94147729107964c1d5bfa0e14
Anthony Tiradani <tiradani@fnal.gov>
2009-08-12 17:21:33 +0000

    added contact string to the lsf tests

--- test/lsf_test.py
+++ test/lsf_test.py
@@ -5,6 +5,7 @@ import unittest
 
 sys.path.append(os.path.expandvars("$GIP_LOCATION/lib/python"))
 from gip_common import config
+from gip_ldap import read_ldap
 from gip_testing import runTest, streamHandler
 
 class TestLsfDynamic(unittest.TestCase):
@@ -23,10 +24,25 @@ class TestLsfDynamic(unittest.TestCase):
         fd = os.popen(path)
         fd.read()
         self.assertEquals(fd.close(), None)
+    
+    def test_contact_string(self):
+        os.environ['GIP_TESTING'] = '1'
+        path = os.path.expandvars("$GIP_LOCATION/libexec/osg_info_provider_" \
+            "lsf.py --config=test_configs/red.conf")
+        fd = os.popen(path)
+        entries = read_ldap(fd)
+        self.failUnless(fd.close() == None)
+
+        for entry in entries:
+            if 'GlueCE' in entry.objectClass:
+                contact_string = entry.glue['CEInfoContactString']
+                self.failIf(contact_string == "", "Contact string is missing")
+                self.failIf(contact_string.endswith("jobmanager-lsf"), \
+                    "Contact string must include the queue.")
 
 def main():
     """
-    The main entry point for when pbs_test is run in standalone mode.
+    The main entry point for when lsf_test is run in standalone mode.
     """
     cp = config("test_configs/red.conf")
     stream = streamHandler(cp)

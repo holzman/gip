5f2e2f5ac1264441a8cf650e48b427405cdf7eee
Burt Holzman <burt@fnal.gov>
2011-10-26 06:04:44 +0800

    Changes to make_release script to use git

--- make_release.sh
+++ make_release.sh
@@ -2,38 +2,50 @@
 
 set -o noglob
 usage='usage: make_release.sh releasename'
-
-if [ -z "$GIP_LOCATION" ] ; then
-    echo 'please set GIP_LOCATION before proceeding'
+tag_regex="*1.3*"
+if [ -z "$GIP_SRC_LOCATION" ] ; then
+    echo 'please set GIP_SRC_LOCATION before proceeding'
     exit 1
 fi
 
-tagdir="${GIP_LOCATION}/tags"
+cd $GIP_SRC_LOCATION
 
 if [ -z "$1" ] ; then
     echo $usage
     echo "Available releases are: "
     echo
-    ls $tagdir
+    git tag -l $tag_regex
     exit 2
 fi
 
 releasename=$1
 
-if [ ! -d "$tagdir/$releasename" ] ; then
+git show-ref --verify refs/tags/$releasename >& /dev/null
+if [ $? -ne 0 ]; then
     echo "Can't find $releasename.  Choices are:"
     echo
-    ls $tagdir
+    git tag -l $tag_regex
     exit 3
 fi
-
-echo $releasename > ${tagdir}/${releasename}/gip/etc/gip_release.txt
-excludelist="--exclude */.svn --exclude *~ --exclude #*# --exclude *.tar.gz --exclude *.tgz --exclude changelog --exclude gip/var/logs/*.log"
 
 if [ -z "$GIP_RELEASE_LOCATION" ] ; then
     export GIP_RELEASE_LOCATION=/tmp
 fi
 
-tar cfvz ${GIP_RELEASE_LOCATION}/$releasename.tgz -C $tagdir $releasename $excludelist
+tarball=${GIP_RELEASE_LOCATION}/${releasename}.tar
+git archive --format=tar --prefix=$releasename/ $releasename  > ${tarball}
 
-rm -f ${tagdir}/${releasename}/gip/etc/gip_release.txt
+# add release tag into tarball
+tmpdir=`mktemp -d`
+cd $tmpdir
+mkdir -p ${releasename}/gip/etc
+echo $releasename > ${releasename}/gip/etc/gip_release.txt
+tar -r -f ${tarball} ${releasename}/gip/etc/gip_release.txt
+rm -f ${releasename}/gip/etc/gip_release.txt
+cd $GIP_SRC_LOCATION
+rm -rf $tmpdir
+
+gzip -f ${tarball}
+mv ${tarball}.gz ${GIP_RELEASE_LOCATION}/${releasename}.tgz
+
+

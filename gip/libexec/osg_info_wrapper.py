9d6f352a7c9b4c1d22aa2153b46c7e593bf33bbc
Burt Holzman <burt@fnal.gov>
2012-03-19 15:10:38 -0500

    Prepend $GIP_LOCATION/lib/python to path (if set)

--- gip/libexec/osg_info_wrapper.py
+++ gip/libexec/osg_info_wrapper.py
@@ -5,6 +5,10 @@ osg-info-wrapper: Run the generic information provider
 """
 
 import gip.utils.info_main
+import sys
+import os
+if 'GIP_LOCATION' in os.environ:
+    sys.path.insert(0, os.path.expandvars("$GIP_LOCATION/lib/python"))
 
 if __name__ == '__main__':
     gip.utils.info_main.main()
